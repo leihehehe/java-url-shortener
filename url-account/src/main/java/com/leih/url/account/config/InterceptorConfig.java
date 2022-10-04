b9d2be3b1c50c7464356c9ee4e650cc9d70c0ff7
shee0021 <hslscarlett@gmail.com>
2022-10-04 14:21:18 +1100

    add interceptor

---
+++ url-account/src/main/java/com/leih/url/account/config/InterceptorConfig.java
@@ -0,0 +1,16 @@
+package com.leih.url.account.config;
+
+import com.leih.url.common.intercepter.LoginInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                //add paths to be intercepted
+                .addPathPatterns("/api/account/*/**","/api/plan/*/**")
+                .excludePathPatterns("/api/account/*/register","/api/account/*/login","/api/account/*/upload",
+                        "/api/notification/*/captcha","/api/notification/*/send_code");
+    }
+}
