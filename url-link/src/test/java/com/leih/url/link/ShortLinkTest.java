60b721cc5f030aa5b224f11cc85dee66307c5379
shee0021 <hslscarlett@gmail.com>
2022-10-11 21:27:21 +1100

    Update test for short link

--- url-link/src/test/java/com/leih/url/link/ShortLinkTest.java
+++ url-link/src/test/java/com/leih/url/link/ShortLinkTest.java
@@ -4,7 +4,9 @@ import com.google.common.hash.Hashing;
 import com.leih.url.common.util.CommonUtil;
 import com.leih.url.link.component.ShortLinkComponent;
 import com.leih.url.link.dao.LinkGroupRepository;
+import com.leih.url.link.entity.Link;
 import com.leih.url.link.entity.LinkGroup;
+import com.leih.url.link.manager.ShortLinkManager;
 import com.leih.url.link.strategry.ShardingDBConfig;
 import com.leih.url.link.strategry.ShardingTableConfig;
 import lombok.extern.slf4j.Slf4j;
@@ -14,23 +16,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Random;
+
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = LinkApplication.class)
 public class ShortLinkTest {
-  @Autowired
-  ShortLinkComponent shortLinkComponent;
-  @Autowired
-  LinkGroupRepository linkGroupRepository;
+  @Autowired ShortLinkComponent shortLinkComponent;
+  @Autowired LinkGroupRepository linkGroupRepository;
+  @Autowired private ShortLinkManager shortLinkManager;
+
   @Test
   public void shorLinkTest() {
-    String originalUrl =
-        "https://github.com/leihehehe";
+    String originalUrl = "https://github.com/leihehehe";
     String shortLink = shortLinkComponent.createShortLink(originalUrl);
     log.info(shortLink);
   }
+
   @Test
-  public void databaseTest(){
+  public void databaseTest() {
     LinkGroup linkGroup = new LinkGroup();
     linkGroup.setName("test");
     linkGroup.setAccountNo(122223L);
@@ -38,12 +42,34 @@ public class ShortLinkTest {
   }
 
   @Test
-  public void testRandomDBPrefix(){
-    for(int i =0;i<20;i++){
+  public void testRandomDBPrefix() {
+    for (int i = 0; i < 20; i++) {
       log.info(ShardingDBConfig.getRandomDBPrefix());
     }
-    for(int i =0;i<20;i++){
+    for (int i = 0; i < 20; i++) {
       log.info(ShardingTableConfig.getRandomTableSuffix());
     }
+  }
+
+  @Test
+  public void testSaveShortLink() {
+    Random random = new Random();
+    for (int i = 0; i < 10; i++) {
+      String originalUrl = "https://github.com/leihehehe" + i;
+      String code = shortLinkComponent.createShortLink(originalUrl);
+      int accountNo = random.nextInt(100000000);
+      log.info(code);
+      Link link = new Link();
+      link.setCode(code);
+      link.setAccountNo(accountNo);
+      link.setSign(CommonUtil.MD5(originalUrl));
+      link.setDelete(0);
+      shortLinkManager.addShortLink(link);
+    }
+  }
+  @Test
+  public void testFindCode(){
+    Link shortLinkByCode = shortLinkManager.findShortLinkByCode("13TXpI5a");
+    log.info("query: {}",shortLinkByCode);
   }
 }
