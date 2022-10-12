b1355136b3cb673773e4c453d24f9b1448b290c7
shee0021 <hslscarlett@gmail.com>
2022-10-12 16:45:16 +1100

    Create entities for domain and group-query-based link

---
+++ url-link/src/main/java/com/leih/url/link/entity/Domain.java
@@ -0,0 +1,42 @@
+package com.leih.url.link.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "domain")
+public class Domain {
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(name = "domain_type", length = 11)
+  private String domainType;
+
+  @Column(name = "value")
+  private String value;
+
+  @Column(name = "del")
+  private int delete;
+
+  @Column(name = "account_no")
+  private long accountNo;
+
+  @Column(
+      name = "gmt_create",
+      insertable = false,
+      updatable = false,
+      columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+  private Timestamp gmtCreate;
+
+  @Column(
+      name = "gmt_modified",
+      insertable = false,
+      updatable = false,
+      columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+  private Timestamp gmtModified;
+}
