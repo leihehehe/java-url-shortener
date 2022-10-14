4307267ef9354bc0a2f2fbf6779a533725446676
shee0021 <hslscarlett@gmail.com>
2022-10-14 17:44:24 +1100

    add rabbit mq config and short link listener

---
+++ url-common/src/main/java/com/leih/url/common/model/EventMessage.java
@@ -0,0 +1,39 @@
+package com.leih.url.common.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventMessage implements Serializable {
+    /**
+     * Message ID
+     */
+    private String messageId;
+    /**
+     * Event Type
+     */
+    private String eventType;
+    /**
+     * Business ID
+     */
+    private String bizId;
+    /**
+     * Account Number
+     */
+    private Long accountNo;
+    /**
+     * Message Body
+     */
+    private String content;
+    /**
+     * Comments
+     */
+    private String comment;
+}
+
