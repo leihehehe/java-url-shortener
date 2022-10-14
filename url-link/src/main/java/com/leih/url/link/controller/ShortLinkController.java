4a9a4f8e7c69e8c249471a6cc6e21363540a7ee7
shee0021 <hslscarlett@gmail.com>
2022-10-14 18:35:03 +1100

    Basic function and test for creating short link (using rabbit mq)

--- url-link/src/main/java/com/leih/url/link/controller/ShortLinkController.java
+++ url-link/src/main/java/com/leih/url/link/controller/ShortLinkController.java
@@ -1,6 +1,12 @@
 package com.leih.url.link.controller;
 
+import com.leih.url.common.util.JsonData;
+import com.leih.url.link.controller.request.ShortLinkAddRequest;
+import com.leih.url.link.service.ShortLinkService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/api/link/v1/")
 public class ShortLinkController {
-
+    @Autowired
+    ShortLinkService shortLinkService;
+    @PostMapping("add")
+    public JsonData createShortLink(@RequestBody ShortLinkAddRequest request){
+         return shortLinkService.createShortLink(request);
+    }
 }
--- url-link/src/main/java/com/leih/url/link/service/ShortLinkService.java
+++ url-link/src/main/java/com/leih/url/link/service/ShortLinkService.java
@@ -1,7 +1,12 @@
 package com.leih.url.link.service;
 
+import com.leih.url.common.util.JsonData;
+import com.leih.url.link.controller.request.ShortLinkAddRequest;
 import com.leih.url.link.vo.LinkVo;
 
 public interface ShortLinkService {
     LinkVo parseShortLinkCode(String shortLinkCode);
+
+    JsonData createShortLink(ShortLinkAddRequest request);
+
 }
