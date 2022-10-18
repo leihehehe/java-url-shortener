165d270ae669918950099c5bb144e2932a21256d
shee0021 <hslscarlett@gmail.com>
2022-10-18 11:13:57 +1100

    Complete Product & ProductOrder entities and sharding config

---
+++ url-shop/src/main/java/com/leih/url/shop/entity/ProductOrder.java
@@ -0,0 +1,69 @@
+package com.leih.url.shop.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "product_order",uniqueConstraints = {
+        @UniqueConstraint(name = "uk_query",columnNames = {"order_no","account_no"})
+})
+public class ProductOrder {
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(name = "product_id")
+  private Long productId;
+  @Column(name = "product_name",length = 64)
+  private String productName;
+  @Column(name = "product_price")
+  private Double productPrice;
+  @Column(name = "product_snapshot",length = 2048)
+  private String productSnapshot;
+  @Column(name = "by_num")
+  private Integer buyNum;
+  @Column(name = "order_no",length = 64)
+  private String orderNo;
+  @Column(name = "state",length = 11)
+  private String state;
+  @Column(name = "create_time")
+  private Timestamp createTime;
+  @Column(name = "total_price")
+  private Double totalPrice;
+  @Column(name = "pay_price")
+  private Double payPrice;
+  @Column(name = "payment_type",length = 64)
+  private String paymentType;
+  @Column(name = "nickname",length = 64)
+  private String nickname;
+  @Column(name = "account_no")
+  private Long accountNo;
+  @Column(name = "del")
+  private Integer del;
+  @Column(
+          name = "gmt_create",
+          insertable = false,
+          updatable = false,
+          columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+  private Timestamp gmtCreate;
+
+  @Column(
+          name = "gmt_modified",
+          insertable = false,
+          updatable = false,
+          columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+  private Timestamp gmtModified;
+  @Column(name = "bill_type",length = 32)
+  private String billType;
+  @Column(name = "bill_header",length = 200)
+  private String billHeader;
+  @Column(name = "bill_content",length = 200)
+  private String billContent;
+  @Column(name = "bill_receiver_phone",length = 32)
+  private String billReceiverPhone;
+  @Column(name = "bill_receiver_email",length = 200)
+  private String billReceiverEmail;
+}
---
+++ url-shop/src/test/java/ShopTest.java
@@ -0,0 +1,16 @@
+import com.leih.url.shop.ShopApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ShopApplication.class)
+@Slf4j
+public class ShopTest {
+    @Test
+    public void test(){
+
+    }
+}
