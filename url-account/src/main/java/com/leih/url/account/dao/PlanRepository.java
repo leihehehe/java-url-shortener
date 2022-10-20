2843f30b42178bec95a11bfd1e30f0c0dd43fb7c
shee0021 <hslscarlett@gmail.com>
2022-10-21 10:28:42 +1100

    Implement plan manager

--- url-account/src/main/java/com/leih/url/account/dao/PlanRepository.java
+++ url-account/src/main/java/com/leih/url/account/dao/PlanRepository.java
@@ -1,10 +1,24 @@
 package com.leih.url.account.dao;
 
 import com.leih.url.account.entity.Plan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface PlanRepository extends JpaRepository<Plan,Long> {
+    @Query("select p from Plan p where p.accountNo=:accountNo and p.expiredDate<:date order by p.gmtCreate desc")
+    Page<Plan> findAvailablePlans(@Param("accountNo") Long accountNo, @Param("date") String date, Pageable pageable);
+    Plan findPlanByAccountNoAndId(Long accountNo, Long planId);
 
+    @Transactional
+    @Modifying
+    @Query("update Plan set dayUsed=:dayUsedTimes where id=:planId and accountNo=:accountNo")
+    int updateDayUsedTimes(@Param("planId") Long planId, @Param("accountNo") Long accountNo,@Param("dayUsedTimes") int dayUsedTimes);
 }
