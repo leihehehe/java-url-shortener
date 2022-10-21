d53a2baad91e45b371bd8b2ef6b3a9aab5b02060
shee0021 <hslscarlett@gmail.com>
2022-10-22 10:41:53 +1100

    Implement function of free plans for newly registered users; Fix bugs of converting json;

--- url-account/src/main/java/com/leih/url/account/config/RabbitMQConfig.java
+++ url-account/src/main/java/com/leih/url/account/config/RabbitMQConfig.java
@@ -1,17 +1,39 @@
 package com.leih.url.account.config;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Slf4j
+@Data
 public class RabbitMQConfig {
     @Bean
     public MessageConverter messageConverter(){
         return  new Jackson2JsonMessageConverter();
+    }
+
+    private String planEventExchange = "plan.event.exchange";
+    private String planFreeInitQueue = "plan.free_init.queue";
+    private String planFreeInitRoutingKey = "plan.free_init.routing.key";
+
+    @Bean
+    public Exchange planEventExchange(){
+        return new TopicExchange(planEventExchange,true,false);
+    }
+    @Bean
+    public Binding planFreeInitBinding(){
+        return new Binding(planFreeInitQueue, Binding.DestinationType.QUEUE,planEventExchange,planFreeInitRoutingKey,null);
+    }
+    @Bean
+    public Queue planFreeInitQueue(){
+        return new Queue(planFreeInitQueue,true,false,false);
     }
 
 }
