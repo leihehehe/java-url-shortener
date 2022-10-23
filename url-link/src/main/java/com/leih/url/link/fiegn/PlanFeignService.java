de2c0a050c4e99f2529f90f17e94c9c67cb10edc
shee0021 <hslscarlett@gmail.com>
2022-10-23 11:26:29 +1100

    check if user has enough plan amount to shorten a link

---
+++ url-link/src/main/java/com/leih/url/link/fiegn/PlanFeignService.java
@@ -0,0 +1,18 @@
+package com.leih.url.link.fiegn;
+
+import com.leih.url.common.util.JsonData;
+import com.leih.url.link.controller.request.UsePlanRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "url-account-service")
+public interface PlanFeignService {
+    /**
+     * Use plan
+     * @param request
+     * @return
+     */
+    @PostMapping(value = "/api/plan/v1/use",headers = {"rpc-token=${rpc.token}"})
+    JsonData usePlan(@RequestBody UsePlanRequest request);
+}
